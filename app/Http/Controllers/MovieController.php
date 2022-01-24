89aacd69874db55b73806761d4c6ecf38e455a13
MorenaMastromarino <morena.mastromarino98@gmail.com>
2022-01-24 18:34:28 +0100

    fix card e prove ricerche commentate

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,8 +10,22 @@ class MovieController extends Controller
     public function index(){
 
         $movies = Movie::all();
-    
+        
+        // film usciti nel 1994
+        // $movies = Movie::where('date','LIKE','1994%')->get();
 
+        // film con voto maggiore di 8 in ordine alfabetico
+        // $movies = Movie::where('vote', '>', '9')
+        //     ->orderBy('title')->get();
+
+        // ricerca con first
+        // $movie = Movie::where('nationality', 'american')->first();
+
+        // ricerca per id con find
+        // $movie = Movie::find(5);    
+
+        // return view('movies', compact('movies', 'movie'));
+        
         return view('movies', compact('movies'));
     }
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,20 +5,28 @@
   <div class="container">
     <h1 class="text-center my-3">Film</h1>
 
-    <div class="row">
+    {{-- <p>Il film è: {{$movie->title}}</p> --}}
+
+    <div class="row g-3">    
+
       
       @forelse ($movies as $movie )
-      <div class="col-3 card">
-        <h3>{{$movie->title}}</h3>
-        <p><strong>Titolo originale: </strong>{{$movie->original_title}}</p>
-        <p><strong>Nazionalità: </strong>{{$movie->nationality}}</p>
-        <p><strong>Anno: </strong>{{$movie->date}}</p>
-        <p><strong>Voto: </strong>{{$movie->vote}}</p>
+      <div class="col-3">
+        <div class="card my-3 p-3">
+          <h3>{{$movie->title}}</h3>
+          <p><strong>Titolo originale: </strong>{{$movie->original_title}}</p>
+          <p><strong>Nazionalità: </strong>{{$movie->nationality}}</p>
+          <p><strong>Anno: </strong>{{$movie->date}}</p>
+          <p><strong>Voto: </strong>{{$movie->vote}}</p>
+
+        </div>
         
       </div>
+
       @empty
       <h1>Non ci sono risultati</h1>
       @endforelse
+
     </div>
   </div>
 
